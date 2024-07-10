1793b0ea3d22d111e3c97698f43f316517d3eb38
maksimd <dvinskii.maxim@gmail.com>
2024-07-10 10:09:40 +0500

    basejava.HW1. Methods implementation

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,63 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid == uuid) {
+                storage[i] = null;
+            }
+        }
+
+        int nonNullIndex = 0;
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[nonNullIndex++] = storage[i];
+            }
+        }
+
+        storage[nonNullIndex] = null;
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                break;
+            }
+            count++;
+        }
+        return count;
     }
 }
