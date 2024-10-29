702f0deaeec57b82f94a7995507e9e6eec3f838e
maksimd <dvinskii.maxim@gmail.com>
2024-10-29 23:40:06 +0500

    Lesson11 HW - Logic modification

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -2,34 +2,34 @@ package ru.javawebinar.basejava;
 
 public class MainDeadLock {
 
-    public static final Object firstLock = new Object();
-    public static final Object secondLock = new Object();
+    public static final Object FIST_LOCK = new Object();
+    public static final Object SECOND_LOCK = new Object();
 
     public static void main(String[] args) {
 
         Thread thread1 = new Thread(() -> {
-            synchronized (firstLock) {
+            synchronized (FIST_LOCK) {
                 System.out.println(Thread.currentThread().getName() + " was started");
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                synchronized (secondLock) {
+                synchronized (SECOND_LOCK) {
                     System.out.println(Thread.currentThread().getName() + " was finished");
                 }
             }
         });
 
         Thread thread2 = new Thread(() -> {
-            synchronized (secondLock) {
+            synchronized (SECOND_LOCK) {
                 System.out.println(Thread.currentThread().getName() + " was started");
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                synchronized (firstLock) {
+                synchronized (FIST_LOCK) {
                     System.out.println(Thread.currentThread().getName() + " was finished");
                 }
             }
