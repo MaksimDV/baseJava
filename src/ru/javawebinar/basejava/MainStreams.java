eeedcc41f7c2f303c94216a01b304cb2ed517875
maksimd <dvinskii.maxim@gmail.com>
2024-11-10 22:06:04 +0500

    Lesson12 - HW

---
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -0,0 +1,35 @@
+package ru.javawebinar.basejava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStreams {
+    public static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (acc, num) -> acc * 10 + num);
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        long oddCount = integers.stream().filter(n -> n % 2 != 0).count();
+
+        return integers.stream()
+                .filter(n -> (oddCount % 2 == 0) == (n % 2 != 0))
+                .collect(Collectors.toList());
+    }
+    public static void main(String[] args) {
+        int[] array1 = {1,2,3,3,2,3};
+        int[] array2 = {9,8};
+        List<Integer> list1 = new ArrayList<>(List.of(1, 1, 1, 2, 2, 1));
+        List<Integer> list2 = new ArrayList<>(List.of(1, 1, 1, 2, 2));
+
+
+        System.out.println(minValue(array1));
+        System.out.println(minValue(array2));
+        System.out.println(oddOrEven(list1));
+        System.out.println(oddOrEven(list2));
+    }
+}
