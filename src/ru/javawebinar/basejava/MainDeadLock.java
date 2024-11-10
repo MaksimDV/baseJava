dc848a270c2ef9522e495f0e37ec4cff73b3eeb3
maksimd <dvinskii.maxim@gmail.com>
2024-11-10 20:40:37 +0500

    Lesson11 HW - Code refactoring (add common method)

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -2,40 +2,28 @@ package ru.javawebinar.basejava;
 
 public class MainDeadLock {
 
-    public static final Object FIST_LOCK = new Object();
+    public static final Object FIRST_LOCK = new Object();
     public static final Object SECOND_LOCK = new Object();
 
     public static void main(String[] args) {
-
-        Thread thread1 = new Thread(() -> {
-            synchronized (FIST_LOCK) {
-                System.out.println(Thread.currentThread().getName() + " was started");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                synchronized (SECOND_LOCK) {
-                    System.out.println(Thread.currentThread().getName() + " was finished");
-                }
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            synchronized (SECOND_LOCK) {
-                System.out.println(Thread.currentThread().getName() + " was started");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                synchronized (FIST_LOCK) {
-                    System.out.println(Thread.currentThread().getName() + " was finished");
-                }
-            }
-        });
+        Thread thread1 = new Thread(() -> executeSynchronizedTask(FIRST_LOCK, SECOND_LOCK));
+        Thread thread2 = new Thread(() -> executeSynchronizedTask(SECOND_LOCK, FIRST_LOCK));
 
         thread1.start();
         thread2.start();
+    }
+
+    private static void executeSynchronizedTask(Object lock1, Object lock2) {
+        synchronized (lock1) {
+            System.out.println(Thread.currentThread().getName() + " was started");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            synchronized (lock2) {
+                System.out.println(Thread.currentThread().getName() + " was finished");
+            }
+        }
     }
 }
